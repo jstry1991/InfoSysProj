3d4fd6c0189e343cfa9fc1a4a85fc1956f90a547
jstry1991 <jstry1991@gmail.com>
2017-07-17 13:40:39 -0400

    modified for weekly schedule
    prints weekly schedule form. slight modification from original php code

--- test.php
+++ test.php
@@ -2,19 +2,29 @@
 <html>
 <body>
 <?php
-include('dbconnect.php');
-$query = "SELECT appointmentID, cost,roomNumber FROM appointments";
+include('dbconnect.php'); 
+$query ="SELECT e.name,e.phoneNumber,s.EmergencyCall,s.date,s.Mon,s.Tue,s.Wed,s.Thu,s.Fri,s.Sat,s.Sun FROM employee e, schedule s WHERE e.employeeID=s.employeeID";
 $result = mysqli_query($conn,$query); 
 echo 
 "<table>
 <tr>
-<th>appointmentID</th>
-<th>Cost</th>
-<th>roomNumber</th>
+<th>name</th>
+<th>phoneNumber</th>
+<th>EmergencyCall</th>
+<th>date</th>
+<th>Monday</th>
+<th>Tuesday</th>
+<th>Wednesday</th>
+<th>Thursday</th>
+<th>Friday</th>
+<th>Saturday</th>
+<th>Sunday</th>
 </tr>";
 if($result->num_rows>0){
 while($row = mysqli_fetch_assoc($result)){
-	echo "<tr><td>".$row["appointmentID"]."</td><td>".$row["cost"]. "</td><td>".$row["roomNumber"]. "</td></tr>";
+	if($row)
+		echo "<tr><td>".$row["name"]."</td><<td>".$row["phoneNumber"]."</td><td>".$row["EmergencyCall"]."</td><td>".$row["date"]."</td><td>".$row["Mon"]
+	."</td><td>".$row["Tue"]."</td><td>".$row["Wed"]."</td><td>".$row["Thu"]."</td><td>".$row["Fri"]."</td><td>".$row["Sat"]."</td><td>".$row["Sun"]."</td></tr>";
 }}
 else{
 	echo "no results bro"; 
