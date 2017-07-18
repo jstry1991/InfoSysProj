ae100ac994d7f8f2761cdac98ea44e269411e359
jstry1991 <jstry1991@gmail.com>
2017-07-18 10:48:01 -0400

    representation change of db info
    made another while loop. one loops the names while the other loops the
    informaton related to the names so an employee name isnt repeated. Also,
    added condition e.job <> nurse and <> janitor because it is only suppose
    to print professionals and exclude nurse

--- individualPractitionersSchedule.php
+++ individualPractitionersSchedule.php
@@ -18,12 +18,13 @@ while($row = mysqli_fetch_assoc($result)){
 	echo "<tr><td>".$row["name"]."</td>";
 	$query2 = "SELECT a.time, a.day FROM employee e, appointmenttime a, 
 	appointments ap WHERE e.employeeID = ap.employeeID 
-	AND a.appointmentID = ap.appointmentID AND e.job <> 'nurse' AND e.job <> 'janitor' 
-	AND e.name = '".$name."';";
+	AND a.appointmentID = ap.appointmentID AND e.job <> 'nurse' AND e.job <> 'janitor' AND e.name = '".$name."';";
 	$result2 = mysqli_query($conn,$query2); 
 	while($row2 = mysqli_fetch_assoc($result2)){
 		echo "<td>".$row2["time"]."</td><td>".$row2["day"]."</td></tr>";
-}}}
+	}
+}
+}
 else{
 	echo "no results bro"; 
 }
