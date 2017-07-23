9d59a816988d5af37f6fcba7b99cafcd0a822444
RichardLin3 <rslin93@gmail.com>
2017-07-23 19:17:03 -0400

    Fixed PhysiciansStatement & MyDb
    Fixed physiciansStatement.php and mydb, there was a temporary lose of
    the diagnoses table

--- physiciansStatement.php
+++ physiciansStatement.php
@@ -3,23 +3,22 @@
 <body>
 	<?php
 	include('dbconnect.php');
+
 $query ="SELECT c.name as clinic_name, c.address as clinic_address, c.phoneNumber as clinic_number, em.taxID as employee_tax, em.name as employee_name, p.name as patient_name, a.visitType, pr.name as procedure_name, pr.codes, pr.other, pr.otherFee, pr.fees as procedure_fees, pr.otherCodes, f.amountPaid, f.totalBalance, i.company
-FROM clinic as c 
-INNER JOIN appointments as a ON c.appointmentID=a.appointmentID
-INNER JOIN employee as em ON a.employeeID=em.employeeID
-INNER JOIN patient as p ON p.patientID=a.patientID
-INNER JOIN fees as f ON f.patientID=p.patientID
-INNER JOIN procedures as pr ON pr.procedureID=a.procedureID
-INNER JOIN insurance as i ON i.patientID=p.patientID UNION SELECT d.name as diagnoses_name, d.codes as diagnoses_codes, d.other as diagnoses_other FROM diagnoses as d";
-/*
+		FROM clinic as c 
+		INNER JOIN appointments as a ON c.appointmentID=a.appointmentID
+		INNER JOIN employee as em ON a.employeeID=em.employeeID
+		INNER JOIN patient as p ON p.patientID=a.patientID
+		INNER JOIN fees as f ON f.patientID=p.patientID
+		INNER JOIN procedures as pr ON pr.procedureID=a.procedureID
+		INNER JOIN insurance as i ON i.patientID=p.patientID";
+
 $query2 = "SELECT d.name as diagnoses_name, d.codes as diagnoses_codes, d.other as diagnoses_other FROM diagnoses as d";
-*/
 $result = mysqli_query($conn,$query);
-//$result2 = mysqli_query($conn,$query2);
+$result2 = mysqli_query($conn,$query2);
 echo
 "<table>
 	<tr>
-<<<<<<< HEAD
 		<th>Clinic Name |</th>
 		<th>Clinic Address |</th>
 		<th>Clinic Phone Number |</th>
@@ -37,31 +36,13 @@ echo
 		<th>Balance Due |</th>
 		<th>Patient's Insurance Company |</th>
 		<th>COmmon Diagnoses </th>
-=======
-		<th>Clinic Name</th>
-		<th>Clinic Address</th>
-		<th>Clinic Phone Number</th>
-		<th>Patient Name</th>
-		<th>Employee Name</th>
-		<th>Tax Identification Number</th>
-		<th>Visit Type</th>
-		<th>Procesures That are Preformed</th>
-		<th>Procedure Code</th>
-		<th>Other</th>
-		<th>Other Fees</th>
-		<th>Other Codes</th>
-		<th>Fee</th>
-		<th>Amount Paid</th>
-		<th>Balance Due</th>
-		<th>Patient's Insurance Company</th>
->>>>>>> origin/localrepo
 		</tr>";
 if ($result->num_rows > 0) {
 while ($row = mysqli_fetch_assoc($result)){ //Creates a loop through results
 	if ($row)
-		echo "<tr><td>".$row["clinic_name"]."</td><td>".$row["clinic_address"]."</td><td>".$row["clinic_number"]."</td><td>".$row["patient_name"]."</td><td>".$row["employee_name"]."</td><td>".$row["employee_tax"]."</td><td>".$row["visitType"]."</td><td>".$row["procedure_name"]."</td><td>".$row["codes"]."</td><td>".$row["other"]."</td><td>".$row["otherFee"]."</td<td>".$row["otherCodes"]."</td>><td>".$row["procedure_fees"]."</td><td>".$row["amountPaid"]."</td><td>".$row["totalBalance"]."</td><td>".$row["company"]."</td><td>".$row["diagnoses_name"]."</td><td>".$row["diagnoses_codes"]."</td><td>".$row["diagnoses_other"]."</td></tr>";
+		echo "<tr><td>".$row["clinic_name"]."</td><td>".$row["clinic_address"]."</td><td>".$row["clinic_number"]."</td><td>".$row["patient_name"]."</td><td>".$row["employee_name"]."</td><td>".$row["employee_tax"]."</td><td>".$row["visitType"]."</td><td>".$row["procedure_name"]."</td><td>".$row["codes"]."</td><td>".$row["other"]."</td><td>".$row["otherFee"]."</td<td>".$row["otherCodes"]."</td>><td>".$row["procedure_fees"]."</td><td>".$row["amountPaid"]."</td><td>".$row["totalBalance"]."</td><td>".$row["company"]."</td></tr>";
 	}
-	/*
+	
 echo 
 	"<tr> 
 		<th>COMMON DIAGNOSES</th>
@@ -69,7 +50,7 @@ echo
 while ($row = mysqli_fetch_assoc($result2)){
 	if ($row)
 		echo "<tr><td>".$row["diagnoses_name"]."</td><td>".$row["diagnoses_codes"]."</td><td>".$row["diagnoses_other"]."</td></tr>";
-}*/
+}
 }
 else {
 	echo "DIDN'T FIND ANYTHING";
