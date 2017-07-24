7e83386b3cfaaa32d5cee918f23ac12cfc86b5a1
jstry1991 <jstry1991@gmail.com>
2017-07-24 12:44:24 -0400

    form fixed for prescription label and receipt

--- prescriptionLabelandReceipt.php
+++ prescriptionLabelandReceipt.php
@@ -8,7 +8,7 @@
 $query ="SELECT med.rxNumber, med.drugName as med_name, med.dateFilled, med.directions, med.form, med.numberOfRefills, med.quantity, med.strength, med.originalDate, p.name as p_name, p.address, em.name as em_name, ph.name as ph_name, di.cost, di.description, di.warning, ins.company, ins.policyType FROM medication as med 
 INNER JOIN patient as p ON med.patientID= p.patientID
 INNER JOIN employee as em ON med.employeeID= em.employeeID
-INNER JOIN pharmacist as ph on med.pharmacistID=ph.pharmacistID
+INNER JOIN employee as ph on med.pharmacistID=ph.employeeID
 INNER JOIN druginfo as di on med.drugID= di.drugID
 INNER JOIN insurance as ins on ins.drugID=di.drugID";
 $result = mysqli_query($conn,$query);
@@ -36,7 +36,10 @@ echo
 if ($result->num_rows > 0) {
 while ($row = mysqli_fetch_assoc($result)){ //Creates a loop through results
 	if ($row)
-		echo "<tr><td>".$row["p_name"]."</td><td>".$row["address"]."</td><td>".$row["em_name"]."</td><td>".$row["rxNumber"]."</td><td>".$row["med_name"]."</td><td>".$row["directions"]."</td><td>".$row["originalDate"]."</td><td>".$row["dateFilled"]."</td><td>".$row["numberOfRefills"]."</td><td>".$row["strength"]."</td><td>".$row["quantity"]."</td><td>".$row["ph_name"]."</td><td>".$row["cost"]."</td><td>".$row["description"]."</td><td>".$row["warning"]."</td><td>".$row["company"]."</td><td>".$row["policyType"]."</td></tr>";
+		echo "<tr><td>".$row["p_name"]."</td><td>".$row["address"]."</td><td>".$row["em_name"]."</td><td>".$row["rxNumber"]."</td><td>"
+	.$row["med_name"]."</td><td>".$row["directions"]."</td><td>".$row["originalDate"]."</td><td>".$row["dateFilled"]."</td><td>"
+	.$row["numberOfRefills"]."</td><td>".$row["strength"]."</td><td>".$row["quantity"]."</td><td>".$row["ph_name"]."</td><td>".$row["cost"].
+	"</td><td>".$row["description"]."</td><td>".$row["warning"]."</td><td>".$row["company"]."</td><td>".$row["policyType"]."</td></tr>";
 }
 }
 else {
